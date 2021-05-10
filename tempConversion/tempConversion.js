1b04eeb7792d9a70174519c1120248c87dc44c9a
iVuDang <vdang159@mtroyal.ca>
2021-05-09 23:55:43 -0600

    7/7 tempConversion completed

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,20 @@
-const ftoc = function() {
-
-}
-
-const ctof = function() {
-
-}
+const ftoc = function(f) {
+  return Math.round((f - 32) * (5/9))
+ }
+ 
+ const ctof = function(c) {
+  return Math.round(((c * 9/5) + 32))
+ }
+ 
+ 
+ console.log(ftoc(32)) 
+ // fahrenheit to celsius, should return 0
+ // formula = (f − 32) × 5/9 = 0°C
+ 
+ console.log(ctof(0)) 
+ // celsius to fahrenheit, should return 32
+ // (c × 9/5) + 32 = 32°F
+ 
 
 module.exports = {
   ftoc,
