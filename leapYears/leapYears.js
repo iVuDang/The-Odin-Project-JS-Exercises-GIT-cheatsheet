92e1ea8a9c63bd83f679d92fffec09fb543ddd31
iVuDang <vdang159@mtroyal.ca>
2021-05-09 23:44:29 -0600

    6/7 leapYears completed

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    return year % 4 === 0 && ( year % 100 !== 0 || year % 400 == 0)
+  }
+  
+  console.log(leapYears(2000));
+  // true 
 
-}
-
+  
 module.exports = leapYears
