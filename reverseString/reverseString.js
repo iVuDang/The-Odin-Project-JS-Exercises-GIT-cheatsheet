cab4f3d9b19a47b25ef59066c1e239bb9f6bccdf
iVuDang <vdang159@mtroyal.ca>
2021-05-09 22:38:51 -0600

    3/7 reverseString completed

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split('') // ["h","e","l","l","o"," ","i","V","u","D","a","n","g"]
+                .reverse() // ["g","n","a","D","u","V","i"," ","o","l","l","e","h"]
+                .join(''); 
 }
+
+
+console.log(reverseString('hello iVuDang'))
+// "gnaDuVi olleh"
+
+
+
+
 
 module.exports = reverseString
