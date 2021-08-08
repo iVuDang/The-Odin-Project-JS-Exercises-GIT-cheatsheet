e77256f820f3b9b26088a30e927f4859b7bc1941
iVuDang <vdang159@mtroyal.ca>
2021-08-08 17:30:26 -0600

    attempt for findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,3 +1,43 @@
-let findTheOldest = function() {
-}
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+      const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+      const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+      return oldestAge < currentAge ? currentPerson : oldest
+    })
+  };
+  
+  const getAge = function(birth, death) {
+    if (!death) {                             
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
+  
+  
+  
+  const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+  
+  console.log(findTheOldest(people));
+  // {
+  //     name: "Ray",
+  //     yearOfBirth: 1962,
+  //     yearOfDeath: 2011,
+  //   }
+  
 module.exports = findTheOldest;
