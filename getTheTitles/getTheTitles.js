47e2c4f5f9f9be9ff3f188d9087b31d4a2244b4f
iVuDang <vdang159@mtroyal.ca>
2021-08-08 17:13:38 -0600

    completed getTheTitles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,36 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    return array.map(book => book.title)
+  };
+  
+  
+  const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+  
+  console.log(getTheTitles(books)) // ['Book','Book2'])
+  
+  // remember we can access objects in two ways
+  // object.name
+  // object['name']
+  
+  
+  // SOLUTION 2
+  const getTheTitles2 = function(array) {
+    return array.map(x => x['title'])
+  };
+  
+  console.log(getTheTitles2(books)) // ['Book','Book2'])
+  
+  // x represents each object in the array
+  // map iterates through each object in the array, and applies our arrow function
+  // Array.map() method creates a new array from the results of calling a function for every element
 
-}
-
+  
 module.exports = getTheTitles;
